43e6c73ace591bf2953f8c5cb25973e326033604
Noemie Pierart <noemie.pierart@gmail.com>
2023-07-05 15:31:58 +0200

    Fixed the chat messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,8 @@ class User < ApplicationRecord
     GiverGardenPoint.where(giver_id: self.id, garden_id: garden.id).sum(:nb_of_points)
   end
 
+  def full_name 
+    "#{self.first_name} #{self.last_name}"
+  end
+
 end
