530a4b25146c3bb4e7cb8321f43a9085b93f545e
Noemie Pierart <noemie.pierart@gmail.com>
2023-07-18 17:13:27 +0200

    Fixed chat index bug

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -3,10 +3,13 @@ class ConversationsController < ApplicationController
   before_action :set_user_conversations, only: [:index, :show, :summary_title]
 
   def index
+    @active_conversation = @conversations.last
+    @conversations = @conversations[0..-2]
   end
 
   def show
-    set_active_conversation
+    @active_conversation = Appointment.find(params[:id]).conversation
+    @conversations = @conversations.reject { |c| c.id == @active_conversation.id }
     if @active_conversation.appointment.giver_id == current_user.id || @active_conversation.appointment.gardener_id == current_user.id
       @message = Message.new
     else
@@ -18,14 +21,10 @@ class ConversationsController < ApplicationController
   
   def set_user_conversations
     @conversations = Conversation.involving_user(current_user).with_messages.sort_by { |c| c.messages.last.created_at }.reverse
-    if @active_conversation.present?
-      @conversations = (@conversations << @active_conversation).uniq
-    end
-    
   end
 
   def set_active_conversation
-    @active_conversation = Appointment.find(params[:id]).conversation
+    
   end
 
 end
