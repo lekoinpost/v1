36e2c8a21bb342ec525c07702f417bef952887c2
Noemie Pierart <noemie.pierart@gmail.com>
2026-01-16 15:31:37 +0100

    Replaced Recaptcha by Cloudflare turnstile

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,7 +3,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  prepend_before_action :check_captcha, only: [:create]
 
   
 
@@ -44,20 +43,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   private
-
-  def check_captcha
-    return if verify_recaptcha 
-
-    self.resource = resource_class.new sign_up_params
-    resource.validate # Look for any other validation errors besides reCAPTCHA
-    set_minimum_password_length
-
-    respond_with_navigational(resource) do
-      flash.discard(:recaptcha_error) # We need to discard flash to avoid showing it on the next page reload
-      render :new
-    end
-  end
-
+  
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
